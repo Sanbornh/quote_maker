b51caba5995a2691c19c265a29cc0ec50ed9aba6
dghollingsworth <dghollingsworth@gmail.com>
2014-05-15 19:45:57 -0400

    completed the rough transitions

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,26 +1,48 @@
 $(document).ready(function(){
 	var time = 750;
+	var click = 0;
 
-	$("#arrow_right").click(function(){
-
-    $("#quote_field").animate({
+	function moveHide(target){
+		$(target).animate({
     	marginLeft:"-=20%",
     	opacity:"0"
 		},time);
+	}
 
-    $("#colour_field").animate({
-    	marginLeft:"-=20%",
-    	opacity:"1"
-    },time);
-    
-    //$("#colour_field").css("display","initial",time);
-    	
-		
-		$("#arrow_left").fadeIn(1000);
+	function moveShow(target){
+		$(target).animate({
+	    	marginLeft:"-=20%",
+	    	opacity:"1"
+	    },time);
+	}
 
-		
+	$("#arrow_right").click(function(){
+		$(this).on("down",function(){
+			$(this).css("opacity","0.7");
+		});
 
+		if(click==2){
+			moveHide("#layout_field");
+			moveShow("#submit_field");
+			$("#arrow_right").fadeOut();
+		}
 
-  });
+		if(click==1){
+			moveHide("#colour_field");
+			moveShow("#layout_field");
+			click++;
+		}
+
+		if(click==0){
+			moveHide("#quote_field");
+			moveShow("#colour_field");
+			//$("#arrow_left").fadeIn(1000);
+			click++;	
+		}
+	});
+
+	$("#arrow_left").click(function(){
+
+	})
 
 });
