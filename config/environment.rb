de02f39bcb5396cded6e8853725f2b90b56df6bb
Sanborn <sanbornh@rogers.com>
2014-05-18 17:30:49 -0400

    Created constant for s3 login credentials

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 QuoteMaker::Application.initialize!
+
+
+S3_CREDENTIALS = YAML.load_file("#{Rails.root}/config/s3.yml")
