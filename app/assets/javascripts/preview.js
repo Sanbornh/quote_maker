2d7889433e00008774e192d2f2a0141b4b8d6557
Sanborn <sanbornh@rogers.com>
2014-05-18 14:06:52 -0400

    Refactored previews.

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
 	var background = $('#preview-background').data('background-color');
+	var quote = $('#quote').data('font-color');
+
 	$('#preview-background').css('background-color', background);
+	$('#quote').css('color', quote);
 
 }) 
