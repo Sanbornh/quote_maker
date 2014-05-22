333c2a3ffa3da10355a8dffd46a4771562f70c14
Sanborn <sanbornh@rogers.com>
2014-05-22 16:49:15 -0400

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+ColourScheme.create(
+	background:    "rgb(4,27,38)", 
+	font:      "rgb(238,247,251)", 
+	highlight:   "rgb(33,99,131)",
+)
+
+ColourScheme.create(
+	background:  "rgb(118,17,28)", 
+	font:      "rgb(255,251,247)", 
+	highlight: "rgb(210,157,158)",
+)
+
+ColourScheme.create(
+	background:   "rgb(12,68,29)", 
+	font:      "rgb(249,255,247)", 
+	highlight: "rgb(192,229,180)",
+)
+
+
 
 LayoutScheme.create(layout_parameters: {
 	font: "Cardo-Regular",
