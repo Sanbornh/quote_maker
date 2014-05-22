f16730639a546fee5d133cf1e5087daf7741f26e
Sanborn <sanbornh@rogers.com>
2014-05-21 20:15:19 -0400

    swatches now styled on the fly based on the colour schemes in the db

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -10,61 +10,14 @@ $( document ).ready(function(){
       }, {duration: 300, queue: false}, function() {
     });
 	});
+
+
+	// Colours swatches based on the data coming in from the
+	// colours schemes in the database.
+	var colorsLocations = $('.swatch-color')
+
+	for(i = 0; i < colorsLocations.length; i++ ) {
+		$(colorsLocations[i]).css("background-color", $(colorsLocations[i]).data("color"));
+	};
+
 });
-
-// $(document).ready(function(){
-// 	var time = 750;
-// 	var click = 0;
-
-
-// 	function moveHide(target){
-// 		$(target).animate({
-//     	marginLeft:"-=60%",
-//     	opacity:"0"
-// 		},time);
-// 	}
-
-// 	function moveShow(target){
-// 		$(target).animate({
-// 	    	marginLeft:"-=60%",
-// 	    	opacity:"1"	    	
-// 	    },time);
-// 	}
-
-// 	$("#arrow_right").click(function(){
-
-// 		if(click==2){
-// 			moveHide("#layout_field");
-// 			$("submit_field").css("display","auto");
-// 			moveShow("#submit_field");
-// 			$("#arrow_right").fadeOut();
-// 		}
-
-// 		if(click==1){
-// 			moveHide("#colour_field");
-// 			moveShow("#layout_field");
-// 			click++;
-// 		}
-
-// 		if(click==0){
-// 			moveHide("#quote_field");
-// 			moveShow("#colour_field");
-// 			//$("#arrow_left").fadeIn(1000);
-// 			click++;	
-// 		}
-// 	});
-
-// 	function layout_selection(image, radio_id){
-// 		$(image).click(function(){
-// 			$(radio_id).attr('checked',true);
-// 			$(this).addClass("glow");
-// 		});
-// 	}
-
-// 	// layout_selection("#top","#wallpaper_layout_scheme_1");
-// 	// layout_selection("#middle","#wallpaper_layout_scheme_2");
-// 	// layout_selection("#bottom","#wallpaper_layout_scheme_3");
-
-
-
-// });//doc ready
