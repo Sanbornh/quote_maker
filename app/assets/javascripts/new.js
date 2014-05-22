00304b49352ccd7c23a53c18eb75a73779314622
Sanborn <sanbornh@rogers.com>
2014-05-21 21:32:54 -0400

    Created selection function for swatches

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -20,4 +20,24 @@ $( document ).ready(function(){
 		$(colorsLocations[i]).css("background-color", $(colorsLocations[i]).data("color"));
 	};
 
+	// This function handles user selection of a colour scheme
+	// by filling a hidden form field with the id of the 
+	// swatch that is clicked on by the user.
+	$('.swatch').click(function(env) {
+
+		var clicked = env.currentTarget
+		var swatches = $('.swatch')
+
+		$('#wallpaper_colour_scheme_id').val($(clicked).data('color-scheme-id'));
+		$(clicked).addClass( 'selected' );
+
+		for(i = 0; i < swatches.length; i++ ) {
+			console.log(i + $(swatches[i]))
+			if($(swatches[i]).hasClass('selected') && (swatches[i] != this)) {
+				$(swatches[i]).removeClass( 'selected' );
+			}
+		};
+
+	});
+
 });
