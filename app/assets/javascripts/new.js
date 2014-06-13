ac17a4133e5a8024d5877a899e60c17bd87488fc
Sanborn <sanbornh@rogers.com>
2014-06-13 15:01:11 -0400

    Refactored code generally and cleaned up selectwallpaperattribute method

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,87 +1,80 @@
-var currentField = 0;
-var formFields = [
-	"#quote-box-wrapper",
-	"#citation-box-wrapper",
-	"#swatches-wrapper",
-	"#layout-thumbs-wrapper",
-	"#generate-wrapper"
-];
-
 // Currently handles selecting an attribute by A) highlighting the thing clicked on
 // and B) adding it's value to a hidden form.
-// This should likely be broken into several functions.
-$.fn.selectWallpaperAttribute = function(formField, formValue, outlineClass){
+// This should likely be broken into two separate functions.
+$.fn.selectWallpaperAttribute = function (formField, formValue, outlineClass){
 	var divOptions = this;
 
-	$(divOptions).click(function(env) {
+	divOptions.click(function () {
+		var clicked = this
 
-		var clicked = env.currentTarget
-		var swatches = $(divOptions)
+		$(formField).val($(clicked).data(formValue)); // Add swatch value to form
 
-		$(formField).val($(clicked).data(formValue));
-		$(clicked).addClass( outlineClass );
-
-		for(i = 0; i < swatches.length; i++ ) {
-			if($(swatches[i]).hasClass(outlineClass) && (swatches[i] != this)) {
-				$(swatches[i]).removeClass(outlineClass);
+		$(clicked).addClass(outlineClass);            // Outline swatch
+		// Ensure only one swatch is selected at a time
+		for(i = 0; i < divOptions.length; i++) {
+			if($(divOptions[i]).hasClass(outlineClass) && (divOptions[i] !== clicked)) {
+				$(divOptions[i]).removeClass(outlineClass);
 			}
 		};
 	});
 };
 
-$( document ).ready(function(){
+$( document ).ready(function () {
+
+	var currentField = 0; 
+	var formFields = $('.form-element');
 
 	// Advance form field by one step
-	$("#arrow-right").click(function() {
-		if(currentField != formFields.length - 1) {
+	$("#arrow-right").click(function () {
+		if(currentField !== formFields.length - 1) {
 			$(formFields[currentField]).fadeOut(300).animate({
 				'left': '-=60px'
 			}, {
 				duration: 500, 
 				queue: false
-			}, function() {});
+			});
 
 			$(formFields[currentField + 1]).delay(300).fadeIn(300).animate({
 				'right': '+=60px'
 			}, {
 				duration: 500, 
 				queue: false
-			}, function() {});
+			});
 
 			currentField++;
 		}
 	});
 
 	// Revert form field by one step
-	$("#arrow-left").click(function() {
-		if(currentField != 0 ) {
+	$("#arrow-left").click(function () {
+		if(currentField !== 0 ) {
 			$(formFields[currentField]).fadeOut(300).animate({
 				'right': '-=60px'
 			}, {
 				duration: 500, 
 				queue: false
-			}, function() {});
+			});
 
 			$(formFields[currentField - 1]).delay(300).fadeIn(300).animate({
 				'left': '+=60px'
 			}, {
 				duration: 500, 
 				queue: false
-			}, function() {});
+			});
 
 			currentField--;
 		}
 	});
 
 	// Colours swatches based on the data coming in from the
-	// colours schemes in the database.
+	// colour schemes in the database.
 	var colorsLocations = $('.swatch-color')
 
 	for(i = 0; i < colorsLocations.length; i++ ) {
 		$(colorsLocations[i]).css("background-color", $(colorsLocations[i]).data("color"));
 	};
 
+	// Outlines and populates hidden forms with swatch and layout options
 	$('.swatch').selectWallpaperAttribute('#wallpaper_colour_scheme_id', 'color-scheme-id', 'selected-color');
 	$('.layout-scheme').selectWallpaperAttribute('#wallpaper_layout_scheme_id', 'layout-scheme-id', 'selected-layout');
-	
 });
