abaa18a147bd145b1df0ffba04e3771f9d94a0db
Sanborn <sanbornh@rogers.com>
2014-06-16 20:52:38 -0400

    Refactored download link controls into a toggle function for better readability

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,5 +1,15 @@
 var wallpaperIds = [];
 
+// Show download link when wallpapers are selected
+// Hide download link when no wallpapers are selected
+function toggleDownloadLink() {
+	if(wallpaperIds.length > 0) {
+		$('#download-link').fadeIn('slow');
+	} else {
+		$('#download-link').fadeOut('slow');
+	}
+}
+
 $( document ).ready(function(){
 
 	// When a thumb is clicked a border is drawn 
@@ -7,6 +17,8 @@ $( document ).ready(function(){
 	// and the id of that wallpaper is added to an array.
 	// Clicking again on the same thumbnail removes the 
 	// border and removes the id from the array.
+	// Note that the actuall submission is done through a 
+	// hidden field on the navbar.
 	$('.wallpaper-img').click(function(){
 		var id = $(this).data('wallpaper-id');
 
@@ -20,15 +32,6 @@ $( document ).ready(function(){
 		};
 
 		$('#ids').val(wallpaperIds);
-
-		if(wallpaperIds) {
-			$('#download-link').fadeIn('slow');
-		}
-
-		if(wallpaperIds.length == 0) {
-			$('#download-link').fadeOut('slow');
-		}
-
-
+		toggleDownloadLink();
 	});
 });
